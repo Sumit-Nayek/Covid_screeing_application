0ea441aa57ee44e64d1c70e2424b6007590ab8d4
sumit nayek <63765654+samartha007@users.noreply.github.com>
2023-10-13 19:48:47 +0530

    Update updated_my_streamlit_app.py

--- updated_my_streamlit_app.py
+++ updated_my_streamlit_app.py
@@ -2,10 +2,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 from pickle import load
-# import base64
-# import sys, path
-# dir = path.Path(__file__)
-# sys.path.append(dir.parent.parent)
+
 def header():
     custom_css = """
         <style>
@@ -21,69 +18,132 @@ def header():
 
     head = """
         <h1 class="header">
-            <font color="#2d00f7">Covid</font>
+            <font color="#2d00f7">Web</font>
             <font color="#6a00f4">-</font>
-            <font color="#8900f2">19</font>
+            <font color="#8900f2">enable</font>
             <font color="#a100f2">Diagonosis</font>
+            <font color="#b100e8">for</font>
+            <font color="#d100d1">COVID</font>
+            <font color="#e500a4">-</font>
+            <font color="#f20089">19</font>
         </h1>
     """
     st.markdown(custom_css, unsafe_allow_html=True)
     st.markdown(head, unsafe_allow_html=True)
 header()
-st.write("Results of Research work funded by ICMR ")
-#col1, col2, col3 = st.columns(3)
-
+# Create two columns for the first two panels
+col1, col2 = st.columns(2)
 USER_INPUT = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+###
 def process_input(input_value):
     result = input_value
     return result
 
-# Age Column.
-# with col1:
-#     AGE = st.number_input("Age", value=0)
-#     USER_INPUT[0] = process_input(AGE)
-AGE = st.number_input("Age", format="%.f")
-USER_INPUT[0] = process_input(AGE)
-# Bmi Column.
-# with col1:
-    # E_gene = st.number_input('CT value E gene', step=1.,format="%.f")
-    # USER_INPUT[1] = process_input(E_gene)
-E_gene = st.number_input('CT value E gene', step=1.,format="%.f")
-USER_INPUT[1] = process_input(E_gene)
-# HbA1c Column.
-# with col1:
-    # pre_medical = st.selectbox('Premedical Condition', ('Yes', 'No'))
-    # USER_INPUT[2] = process_input(pre_medical)
-pre_medical = st.selectbox('Premedical Condition', ('Yes', 'No'))
-USER_INPUT[2] = process_input(pre_medical)
-# Gender Column.
-# with col1:
-    # gender = st.selectbox('Gender', ('Male', 'Female'))
-    # USER_INPUT[3] = process_input(gender)
-gender = st.selectbox('Gender', ('Male', 'Female'))
-USER_INPUT[3] = process_input(gender)
+# Panel 1: Left panel
+with col1:
+      st.markdown(
+          """
+          <div style="
+              background-color: #ffdb4d;
+              padding: 5px,3px;
+              border: 1px solid #00FF00;
+              border-radius: 5px;text-align: center;
+          ">
+          <h3 style="color: ##00FF00;">Input Data</h3>
 
-symptoms = ['fever','cough','breathlessness','body_ache','vomiting','sore_throat','diarrhoea','sputum','nausea','nasal_discharge','loss_of_taste','loss_of_smell','abdominal_pain','chest_pain','haemoptsis','head_ache','body_pain','weak_ness',
- 'cold']
+          </div>
+          """,
+          unsafe_allow_html=True,
+      )
+      patient_name =st.text_input('Name')
+      AGE = st.number_input("Age", format="%.f")
+      USER_INPUT[0] = process_input(AGE)
+      E_gene = st.number_input('CT value E gene', step=1.,format="%.f")
+      USER_INPUT[1] = process_input(E_gene)
+      pre_medical = st.selectbox('Premedical Condition', ('Yes', 'No'))
+      USER_INPUT[2] = process_input(pre_medical)
+      gender = st.selectbox('Gender', ('Male', 'Female'))
+      USER_INPUT[3] = process_input(gender)
+# Panel 2: Middle panel
+with col2:
+      st.markdown(
+          """
+          <div style="
+              background-color: #ffdb4d;
+              padding: 5px,3px;
+              border: 1px solid #00FF00;
+              border-radius: 5px;text-align: center;
+          ">
+          <h3 style="color: ##00FF00;">Symptoms selection</h3>
+          </div>
+          """,
+          unsafe_allow_html=True,
+      )
+      symptoms = ['fever', 'cough', 'breathlessness', 'body_ache', 'vomiting', 'sore_throat',
+                  'diarrhoea', 'sputum', 'nausea', 'nasal_discharge', 'loss_of_taste', 'loss_of_smell',
+                  'abdominal_pain', 'chest_pain', 'haemoptsis', 'head_ache', 'body_pain', 'weak_ness', 'cold']
 
-# Create a DataFrame to store symptom values
-symptom_df = pd.DataFrame(columns=symptoms)
-st.write("Symptoms Set")
-# Create checkboxes for each symptom and update the DataFrame
-for symptom in symptoms:
-    selected = st.checkbox(symptom)
-    symptom_df[symptom] = [1] if selected else [0]
+      # Split the symptoms into two columns with 10 rows in each column
+      symptoms_split = [symptoms[:10], symptoms[10:20], symptoms[20:]]
 
-#USER_INPUT[4] = process_input(SH)
-if USER_INPUT[2] == 'Yes':
-    USER_INPUT[2] = 1
-elif USER_INPUT[2] == 'No':
-    USER_INPUT[2] = 0
-new_data = pd.DataFrame({'Age' : USER_INPUT[0], 'E_gene' : USER_INPUT[1], 'Pre_medical' : USER_INPUT[2]}, index = [0])
-# Concatenate the two DataFrames vertically
-combined_df = pd.concat([new_data, symptom_df], axis=1, ignore_index=True)
+      # Create a DataFrame to store symptom values
+      symptom_df = pd.DataFrame(columns=symptoms)
+
+
+      # Create columns for checkboxes (e.g., 2 columns)
+      coll1, coll2 = st.columns(2)
+
+      # Initialize a dictionary to store symptom values
+      symptom_values = {}
+
+      # Ensure the loop doesn't exceed the length of the split list
+      for i in range(len(symptoms)):
+          with coll1 if i < 10 else coll2:
+              selected = st.checkbox(symptoms[i])
+              symptom_values[symptoms[i]] = 1 if selected else 0
+
+      # Append the symptom values to the DataFrame
+      symptom_df = symptom_df.append(symptom_values, ignore_index=True)
+#              selected = st.checkbox(symptoms_split[2][i])
+#              symptom_df[symptoms_split[2][i]] = [1] if selected else [0]
+
+      #USER_INPUT[4] = process_input(SH)
+      if USER_INPUT[2] == 'Yes':
+          USER_INPUT[2] = 1
+      elif USER_INPUT[2] == 'No':
+          USER_INPUT[2] = 0
+      new_data = pd.DataFrame({'Age' : USER_INPUT[0], 'E_gene' : USER_INPUT[1], 'Pre_medical' : USER_INPUT[2]}, index = [0])
+      # Concatenate the two DataFrames vertically
+      combined_df = pd.concat([new_data, symptom_df], axis=1, ignore_index=True)
+
+
+# Panel 3: Full-width panel
+st.markdown(
+    """
+    <div style="
+        background-color: #ff9933;
+        padding: 5px;
+        border: 1px solid #FFA500;
+        border-radius: 5px;text-align: center;
+    ">
+    <h3 style="color: ##00FF00;">Diagonosis recomendation</h3>
+    </div>
+    """,
+    unsafe_allow_html=True,
+)
+st.write('Sample of the Input')
 st.write(combined_df)
-# combined_df.columns = list(new_data.columns) + list(symptom_df.columns)
+bayes = load(open('content/bayes.pkl', 'rb'))
+logistic = load(open('content/logistic.pkl', 'rb'))
+random_tree =load(open('content/random_tree.pkl', 'rb'))
+svm_linear = load(open('content/svm_linear.pkl', 'rb'))
+svm_rbf = load(open('content/svm_rbf.pkl', 'rb'))
+svm_sigmoid = load(open('content/svm_sigmoid.pkl', 'rb'))
+tree = load(open('content/tree.pkl', 'rb'))
+# Dropdown menu for model selection
+selected_model = st.selectbox('Select a Model', ['Naive Bayes', 'Logistic Regression', 'Decision Tree', 'Random Forest', 'SVM (Linear)', 'SVM (RBF)', 'SVM(Sigmoid)'])
+prediction=0
+# Perform predictions based on the selected model
 
 CSS = """
     <style>
@@ -102,29 +162,12 @@ HEAD_YES = """
 HEAD_NO = """
     <h6 class="header_pred" style="color:#affc41"> You Don't Have Covid-19 </h6>
 """
-# ####################################################### Button For Prediction #####################################################
-
-# submit_button = st.button("Submit")
-# if submit_button:
-#     st.write("Combined DataFrame:")
-#     st.write(combined_df)
-    
-bayes = load(open('content/bayes.pkl', 'rb'))
-logistic = load(open('content/logistic.pkl', 'rb'))
-random_tree =load(open('content/random_tree.pkl', 'rb'))
-svm_linear = load(open('content/svm_linear.pkl', 'rb'))
-svm_rbf = load(open('content/svm_rbf.pkl', 'rb'))
-svm_sigmoid = load(open('content/svm_sigmoid.pkl', 'rb'))
-tree = load(open('content/tree.pkl', 'rb'))
-# Dropdown menu for model selection
-selected_model = st.selectbox('Select a Model', ['Naive Bayes', 'Logistic Regression', 'Decision Tree', 'Random Forest', 'SVM (Linear)', 'SVM (RBF)', 'SVM(Sigmoid)'])
-prediction=0
-# Perform predictions based on the selected model
 
 if st.button('Make Predictions'):
     if selected_model == 'Naive Bayes':
         prediction = bayes.predict(combined_df)
         st.write("Predicted Results:")
+        st.write(f"Fraction Value: {prediction*100}")
         if prediction == 1:
             st.markdown(CSS, unsafe_allow_html=True)
             st.markdown(HEAD_YES, unsafe_allow_html=True)
@@ -137,6 +180,7 @@ if st.button('Make Predictions'):
     elif selected_model == 'Logistic Regression':
         prediction = logistic.predict(combined_df)
         st.write("Predicted Results:")
+        st.write(f"Fraction Value: {prediction*100}")
         if prediction == 1:
             st.markdown(CSS, unsafe_allow_html=True)
             st.markdown(HEAD_YES, unsafe_allow_html=True)
@@ -149,6 +193,7 @@ if st.button('Make Predictions'):
     elif selected_model == 'Decision Tree':
         prediction = tree.predict(combined_df)
         st.write("Predicted Results:")
+        st.write(f"Fraction Value: {prediction*100}")
         if prediction == 1:
             st.markdown(CSS, unsafe_allow_html=True)
             st.markdown(HEAD_YES, unsafe_allow_html=True)
@@ -161,6 +206,7 @@ if st.button('Make Predictions'):
     elif selected_model == 'Random Forest':
         prediction = random_tree.predict(combined_df)
         st.write("Predicted Results:")
+        st.write(f"Fraction Value: {prediction*100}")
         if prediction == 1:
             st.markdown(CSS, unsafe_allow_html=True)
             st.markdown(HEAD_YES, unsafe_allow_html=True)
@@ -173,6 +219,7 @@ if st.button('Make Predictions'):
     elif selected_model == 'SVM (Linear)':
         prediction = svm_linear.predict(combined_df)
         st.write("Predicted Results:")
+        st.write(f"Fraction Value: {prediction*100}")
         if prediction == 1:
             st.markdown(CSS, unsafe_allow_html=True)
             st.markdown(HEAD_YES, unsafe_allow_html=True)
@@ -185,6 +232,7 @@ if st.button('Make Predictions'):
     elif selected_model == 'SVM (RBF)':
         prediction = svm_rbf.predict(combined_df)
         st.write("Predicted Results:")
+        st.write(f"Fraction Value: {prediction*100}")
         if prediction == 1:
             st.markdown(CSS, unsafe_allow_html=True)
             st.markdown(HEAD_YES, unsafe_allow_html=True)
@@ -197,6 +245,7 @@ if st.button('Make Predictions'):
     elif selected_model == 'SVM (Sigmoid)':
         prediction = svm_sigmoid.predict(combined_df)
         st.write("Predicted Results:")
+        st.write(f"Fraction Value: {prediction*100}")
         if prediction == 1:
             st.markdown(CSS, unsafe_allow_html=True)
             st.markdown(HEAD_YES, unsafe_allow_html=True)
