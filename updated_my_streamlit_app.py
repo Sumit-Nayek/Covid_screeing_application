d93ddad1973f8249d3bd6a5d01793353b524c427
sumit nayek <63765654+samartha007@users.noreply.github.com>
2023-11-17 14:33:31 +0530

    Update updated_my_streamlit_app.py
    Changing the color of the Reseach title.

--- updated_my_streamlit_app.py
+++ updated_my_streamlit_app.py
@@ -42,7 +42,7 @@ def header():
     st.markdown(custom_css, unsafe_allow_html=True)
     st.markdown(head, unsafe_allow_html=True)
 header()
-st.write('**A Research Project sponsored by ICMR Govt. of India**')
+st.write("<span style='color: #FFA500; font-weight: bold;'>A Research Project sponsored by ICMR Govt. of India</span>", unsafe_allow_html=True)
 # Create two columns for the first two panels
 col1, col2 = st.columns(2)
 USER_INPUT = [0, 0, 0, 0, 0, 0, 0, 0, 0]
