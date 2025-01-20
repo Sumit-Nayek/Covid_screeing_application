df8e77e99abbdf7efa0965409a734f1fbac6c6c9
Sumit Nayek <63765654+Sumit-Nayek@users.noreply.github.com>
2025-01-20 17:04:03 +0530

    Update testing_new_app.py
    updating the variable name

--- testing_new_app.py
+++ testing_new_app.py
@@ -109,7 +109,7 @@ if page == "Risk Assessment":
     # Input fields
 
     name1 = st.text_input("Name")
-    AGE = st.number_input("Age", step=1.,format="%.f")
+    age = st.number_input("Age", step=1.,format="%.f")
     USER_INPUT[0] = AGE
     gender1 = st.selectbox("Gender", ["Male", "Female", "Other"])
     USER_INPUT[1] = gender1
@@ -248,7 +248,7 @@ if page == "Risk Assessment":
     
           #USER_INPUT[4] = process_input(SH)
 
-          new_data = pd.DataFrame({'Age' : USER_INPUT[0], 'E_gene' : USER_INPUT[3], 'Pre_medical' : USER_INPUT[2]}, index = [0])
+          new_data = pd.DataFrame({'age' : USER_INPUT[0], 'ct_value_rdrp' : USER_INPUT[3], 'underlying_medical_condition' : USER_INPUT[2]}, index = [0])
           # Concatenate the two DataFrames vertically
           combined_df = pd.concat([new_data, symptom_df], axis=1, ignore_index=True)
           new_table_df=combined_df
